12e098019b3637a5ad6d926a4cdb6e3f9a0014bb
Everton Nascimento <nascimentoeverton568@gmail.com>
2023-06-04 18:44:08 -0300

    configurando banco de dados

--- src/main/java/com/urbtech/UrbtechApiApplication.java
+++ src/main/java/com/urbtech/UrbtechApiApplication.java
@@ -3,7 +3,7 @@ package com.urbtech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "config")
 public class UrbtechApiApplication {
 
 	public static void main(String[] args) {
